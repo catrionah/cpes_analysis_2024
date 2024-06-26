b7ad0c14e64bd26363788fd230038e862d253f5a
catrionah <catriona.haddow@phs.scot>
2024-06-26 13:27:18 +0100

    first commit

---
+++ 02.CPES_2024_add_weights.R
@@ -0,0 +1,18 @@
+# Name of file: 02.CPES_2024_add_weights.R
+# 
+# Original author(s): Catriona Haddow 
+#   
+# Written/run on: Posit Workbench - RStudio R 4.1.2
+# 
+# Description of content:  Syntax to add weights to 2024 survey data.  
+# 
+# Approximate run time: 2 min
+# 
+# Approximate memory usage: 7 GiB
+
+###################################################################################
+
+source("00.CPES_2024_set_up_packages.R")
+source("00.CPES_2024_set_up_file_paths.R")
+source("00.CPES_2024_functions.R")
+
